62c289d60a92909c943c7dc9e03f21287055f63b
Erick Garcia <erick-noel_123@outlook.com>
2025-11-06 23:16:32 -0600

    Agregar archivos de proyecto.

---
+++ MoviesApp.Domain/Entities/Director.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesApp.Domain.Entities
+{
+    internal class Director
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+
+        public bool IsActive { get; set; }  
+    }
+}
---
+++ MoviesApp.Domain/Entities/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesApp.Domain.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Genre { get; set; } = string.Empty;  
+        
+        public int Year { get; set; }
+
+    }
+}
